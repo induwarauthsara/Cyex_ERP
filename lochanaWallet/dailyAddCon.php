47b835ed3f5f6f602e60d231163afb7d8a485329
Induwara Uthsara <induwarauthsara@gmail.com>
2024-04-08 15:20:01 +0530

    lochana Wallet

---
+++ lochanaWallet/dailyAddCon.php
@@ -0,0 +1,13 @@
+<?php
+require_once '../inc/config.php';
+
+// Write the SQL query to insert the data
+$amount = 1000;
+$description = 'Day Salary';
+$sql = "INSERT INTO lochana_wallet (amount, description) VALUES ('$amount', '$description');";
+if ($con->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $con->error;
+}
+$con->close();
