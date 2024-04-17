6603f90c5200737e571e9cce6a3ca65900262073
Induwara Uthsara <induwarauthsara@gmail.com>
2024-04-17 18:26:27 +0530

    productList -> product delete feature implemented

---
+++ comboProduct/delete_product-submit.php
@@ -0,0 +1,29 @@
+<?php require_once '../inc/config.php';
+
+
+// Database blueprint
+// items (`id`, `item_name`, `description`, `cost`, `qty`, `supplier`)
+// products (`product_id`, `product_name`, `description`, `stock_qty`, `rate`, `cost`, `profit`, `has_stock`, `image`, `show_in_landing_page`)
+// makeProduct (`id`, `item_name`, `product_name`, `qty`)
+
+$product_id = $_GET['productId'];
+$product_name = $_GET['productName'];
+
+// Delete from makeProduct table
+$sql = "DELETE FROM makeProduct WHERE product_name = '$product_name'";
+if (mysqli_query($con, $sql)) {
+    // echo "Record deleted successfully";
+    $deleted = true;
+} else {
+    echo "Error deleting record: " . mysqli_error($con);
+}
+
+// Delete From products Table
+if ($deleted) {
+    $sql = "DELETE FROM products WHERE product_id = '$product_id'";
+    if (mysqli_query($con, $sql)) {
+        echo "Record deleted successfully";
+    } else {
+        echo "Error deleting record: " . mysqli_error($con);
+    }
+}
