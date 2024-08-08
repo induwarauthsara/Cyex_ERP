db8d247776a65ea86c76c874c8c1142dbaaa265b
Induwara Uthsara <induwarauthsara@gmail.com>
2024-08-08 14:56:30 +0530

    Remove Password edit from ACP HRM

--- AdminPanel/hrm/editEmployee.php
+++ AdminPanel/hrm/editEmployee.php
@@ -12,9 +12,8 @@ $role = $_POST['role'];
 $nic = $_POST['nic'];
 $salary = $_POST['salary'];
 $day_salary = $_POST['day_salary'];
-$password = $_POST['password'];
 
 
 // `employees`(`employ_id`, `emp_name`, `mobile`, `address`, `bank_account`, `role`, `nic`, `salary`, `day_salary`, `password`)
-$sql = "UPDATE `employees` SET `emp_name` = '$emp_name', `mobile` = '$mobile', `address` = '$address', `bank_account` = '$bank_account', `role` = '$role', `nic` = '$nic', `salary` = '$salary', `day_salary` = '$day_salary', `password` = '$password' WHERE `employees`.`employ_id` = {$employ_id}";
+$sql = "UPDATE `employees` SET `emp_name` = '$emp_name', `mobile` = '$mobile', `address` = '$address', `bank_account` = '$bank_account', `role` = '$role', `nic` = '$nic', `salary` = '$salary', `day_salary` = '$day_salary' WHERE `employees`.`employ_id` = {$employ_id}";
 insert_query($sql, "Name: $emp_name", "Update Employee Details");
