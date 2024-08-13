50af72fb87e13555d94b7d60e35816bd982d1151
Induwara Uthsara <induwarauthsara19@gmail.com>
2024-08-13 19:06:17 +0530

    add minus (-) mark for Salary Table when Salary pay

--- AdminPanel/hrm/paySalary.php
+++ AdminPanel/hrm/paySalary.php
@@ -11,7 +11,7 @@ if (isset($_POST['employee_id']) && isset($_POST['amount'])) {
 
     // 1. Add Record to Salary Table
     $description = "Salary Paid";
-    $sql = "INSERT INTO salary (emp_id, amount, description) VALUES ('$employee_id', '$amount', '$description')";
+    $sql = "INSERT INTO salary (emp_id, amount, description) VALUES ('$employee_id', '-$amount', '$description')";
     insert_query($sql, "Employee ID: $employee_id, Rs. $amount", "Employee Salary Paid - Update Salary Table");
 
     // 2. Update Employee Salary
