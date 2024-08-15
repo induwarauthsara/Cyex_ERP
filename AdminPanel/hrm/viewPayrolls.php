e25632deddc07ddbe41b6b8201f9fa632b3c00aa
Induwara Uthsara <induwarauthsara19@gmail.com>
2024-08-15 14:46:46 +0530

    Modify View Payroll Page Button Display Logic

--- AdminPanel/hrm/viewPayrolls.php
+++ AdminPanel/hrm/viewPayrolls.php
@@ -8,7 +8,7 @@ if (isset($_GET['employee_id'])) {
     $sql = "SELECT salary FROM employees WHERE employ_id = '$employee_id'";
 
     $total_salary_have_pay = number_format(mysqli_fetch_assoc(mysqli_query($con, "SELECT SUM(salary) as total_salary FROM employees WHERE employ_id = '$employee_id' AND status = 1"))['total_salary'], 2);
-    
+
     $employee_name = mysqli_fetch_assoc(mysqli_query($con, "SELECT emp_name FROM employees WHERE employ_id = '$employee_id'"))['emp_name'];
     echo "<h1>Payroll for $employee_name</h1>";
     $PayrollTableSql = "SELECT salary_id, employees.emp_name, amount, description, date, time
@@ -19,20 +19,7 @@ if (isset($_GET['employee_id'])) {
 
     echo "<h3 style='color:white'>Total Salary to pay : Rs. $total_salary_have_pay </h3><br>";
 
-    // buttons for viewing all employees names
-    echo "<div style='display: flex; gap: 5px;'>";
-    echo "<h3>Others : </h3>";
-
-    $sql = "SELECT employ_id, emp_name FROM employees WHERE status = 1";
-    $result = mysqli_query($con, $sql);
-    if ($result) {
-        while ($row = mysqli_fetch_assoc($result)) {
-            $employee_id = $row['employ_id'];
-            $employee_name = $row['emp_name'];
-            echo "<a href='viewPayrolls.php?employee_id=$employee_id'><button>$employee_name</button></a><br>";
-        }
-    }
-    echo "</div>";
+    $Employee_names_sql = "SELECT employ_id, emp_name FROM employees WHERE status = 1 AND employ_id != $employee_id";
 } else {
     // for all employees
     $total_salary_have_pay = number_format(mysqli_fetch_assoc(mysqli_query($con, "SELECT SUM(salary) as total_salary FROM employees WHERE status = 1"))['total_salary'], 2);
@@ -41,12 +28,29 @@ if (isset($_GET['employee_id'])) {
                 FROM salary, employees
                 WHERE salary.emp_id = employees.employ_id
                 ORDER BY date DESC, time DESC";
+
+    $Employee_names_sql = "SELECT employ_id, emp_name FROM employees WHERE status = 1";
 }
 
 ?>
 
 <title>Payroll</title>
 
+<!-- Buttons for view all Employee Names -->
+<div style='display: flex; gap: 5px;'>
+    <h3>Others : </h3>
+    <a href='viewPayrolls.php'><button>All</button></a>
+    <?php
+        $Employee_names_sql_result = mysqli_query($con, $Employee_names_sql);
+
+    if ($Employee_names_sql_result) {
+        while ($row = mysqli_fetch_assoc($Employee_names_sql_result)) {
+            $employee_id_for_buttons = $row['employ_id'];
+            $employee_name_for_buttons = $row['emp_name'];
+            echo "<a href='viewPayrolls.php?employee_id=$employee_id_for_buttons'><button>$employee_name_for_buttons</button></a><br>";
+        }
+    } ?>
+</div>
 <table id="DataTable" class="display">
     <thead>
         <tr>
