24eb221de716f6ad3d10ecc5c70b84f09a5c9bb7
Induwara Uthsara <induwarauthsara19@gmail.com>
2025-01-06 09:16:00 +0530

    Feature - Add "Clear Quick Search" button and functionality, update layout for cart buttons

--- index.php
+++ index.php
@@ -31,6 +31,7 @@
                     <button class="view-held-invoices" onclick="loadHeldInvoices()"><svg style="width: 20px; color: #fff;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
                             <path fill-rule="evenodd" d="M2.25 12c0-5.385 4.365-9.75 9.75-9.75s9.75 4.365 9.75 9.75-4.365 9.75-9.75 9.75S2.25 17.385 2.25 12zM9 8.25a.75.75 0 00-.75.75v6c0 .414.336.75.75.75h.75a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75H9zm5.25 0a.75.75 0 00-.75.75v6c0 .414.336.75.75.75H15a.75.75 0 00.75-.75V9a.75.75 0 00-.75-.75h-.75z" clip-rule="evenodd"></path>
                         </svg> &nbsp; Held Invoices List</button>
+                    <button class="shortcut-button" onclick="clearQuickSearch()"><i class="fas fa-broom"></i> &nbsp; Clear Quick Search</button>
                 </div>
             </div>
             <!-- Customer Details -->
@@ -217,6 +218,17 @@
                             </tbody> </table> `,
                 icon: 'info',
                 showConfirmButton: false
+            });
+        }
+
+        function clearQuickSearch() {
+            // Clear Local Storage > productsCache key values
+            localStorage.removeItem('productsCache');
+            Swal.fire({
+                title: 'Quick Search Cleared',
+                text: 'Quick Search Cache Cleared Successfully',
+                icon: 'success',
+                confirmButtonText: 'OK'
             });
         }
 
